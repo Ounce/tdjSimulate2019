9ca71a356592d7a8a8227770c78cd19836ebc858
Ounce <tdj.lwj@163.com>
2020-08-17 14:06:34 +0800

    添加ProfileViewModel类的部分参数。

--- tdjClassLibrary/Profile/ProfileViewModel.cs
+++ tdjClassLibrary/Profile/ProfileViewModel.cs
@@ -5,12 +5,42 @@ using System.Text;
 
 namespace tdjClassLibrary.Profile
 {
-    class ProfileViewModel : NotifyPropertyChanged
+    public class ProfileViewModel : NotifyPropertyChanged
     {
         public ObservableCollection<SlopeViewModel> Slopes;
         public int Count
         {
             get { return Slopes.Count; }
         }
+
+        /// <summary>
+        /// 原始Altitude位置。未设置时此值为-1。
+        /// 读取Excel数据时，以第一个非-999高程为FixAltitudePosition。
+        /// </summary>
+        public int FixAltitudePosition { get; set; }
+
+        /// <summary>
+        /// 原始Altitude。如果原始值为BeginAltitude，则此值为true；如果为EndAltitude时为false。
+        /// </summary>
+        public bool FixBeginOrEndAltitude { get; set; }
+
+        /// <summary>
+        /// 纵断面全长。
+        /// </summary>
+        public double Length
+        {
+            get
+            {
+                double l = 0;
+                foreach (var s in Slopes)
+                {
+                    l += s.Length;
+                }
+                return l;
+            }
+        }
+
     }
+
+
 }
