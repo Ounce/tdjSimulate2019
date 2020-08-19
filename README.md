06e122b7e96694f8775b8c0118a5709d6ac6e75b
Ounce <tdj.lwj@163.com>
2020-08-19 15:44:37 +0800

    添加WindowsDesktop框架；整理基础类。

--- README.md
+++ README.md
@@ -14,6 +14,8 @@
 
 图形的各项参数，设置在图形类中，并在图形中设置数据类。这类图形一般为纵断面折线图、纵断面高程表等；但是这些图形组合成的具有各个项目特有的图形应该在各项目中定义和实现，这些类在项目的Model文件夹内定义。
 
+通过“编辑项目文件”，给tdjClassLibrary项目增加了框架“Microsoft.WindowsDesktop.App.WPF”，在项目文件的Project项中的Sdk改成：Microsoft.NET.Sdk.WindowsDesktop。
+
 ## tdjClassLibrary的命名空间
 
 tdjClassLibrary是库项目，PresentationFramework.dll是手动添加的，这个dll包含了System.Windows.Shapes命名空间，Polyline在这个命名空间里。
--- tdjClassLibrary/Profile/ProfileScaleDrawing.cs
+++ tdjClassLibrary/Profile/ProfileScaleDrawing.cs
@@ -1,13 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Windows.Controls;
 
 namespace tdjClassLibrary.Profile
 {
     /// <summary>
     /// 纵断面比例图。高程比例与里程比例不同。
     /// </summary>
-    public class ScaleDrawing : BaseDrawing
+    public class ProfileScaleDrawing : BaseProfileDrawing
     {
         /// <summary>
         /// 图形顶端对应的高程。
@@ -18,5 +19,7 @@ namespace tdjClassLibrary.Profile
         /// 图形底部对应的高程。
         /// </summary>
         public double BottomAltitude { get; set; }
+
+
     }
 }
