f122d7e70d10a7db0b1832f7a8bf5c5a6b1cf684
Ounce <tdj.lwj@163.com>
2020-08-25 13:15:54 +0800

    添加tdjClassLibrary引用。

---
+++ ProfileDesigner/ProfileDesign.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using tdjClassLibrary;
+
+namespace ProfileDesigner
+{
+    class ProfileDesign :  NotifyPropertyChanged
+    {
+        
+    }
+}
