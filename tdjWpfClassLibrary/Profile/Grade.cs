8c9b5c501457d23eac81fbf381853f0138c30024
DESKTOP-495NC0O\tdjlw <tdj.lwj@163.com>
2020-11-01 21:25:18 +0800

    整理

--- tdjWpfClassLibrary/Profile/Grade.cs
+++ tdjWpfClassLibrary/Profile/Grade.cs
@@ -4,8 +4,21 @@ using System.Text;
 
 namespace tdjWpfClassLibrary.Profile
 {
+    /// <summary>
+    /// 与坡度有关的静态类。
+    /// </summary>
     public static class Grade
     {
+        /// <summary>
+        /// 坡度单位，‰为1000；%为100；国内和欧洲为1000，美国为100。
+        /// </summary>
+        public static int Unit = 1000;
+
+        /// <summary>
+        /// 判断坡度的方向，返回值为-1，反坡；0，平坡；1，顺坡。
+        /// </summary>
+        /// <param name="grade"></param>
+        /// <returns></returns>
         public static int Direction(double grade)
         {
             if (grade < -0.00001) return -1;
