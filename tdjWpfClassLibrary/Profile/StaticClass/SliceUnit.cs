6812d4d7a583efecd58e31c1808f9d8a243bca32
DESKTOP-495NC0O\tdjlw <tdj.lwj@163.com>
2021-01-09 22:57:26 +0800

    完善SliceUnit

--- tdjWpfClassLibrary/Profile/StaticClass/SliceUnit.cs
+++ tdjWpfClassLibrary/Profile/StaticClass/SliceUnit.cs
@@ -5,15 +5,70 @@ using System.Text;
 namespace tdjWpfClassLibrary.Profile.StaticClass
 {
     public enum Measure { Metric, British }
-    class SliceUnit : NotifyPropertyChanged
-    {
 
+    /// <summary>
+    /// 标尺的方向。刻度线的方向与此垂直。
+    /// </summary>
+    public enum SliceDirection { Horizontal, Vertical }
+
+    public class TickMark
+    {
+        public double Length;
+        double X1, X2, Y1, Y2;
     }
 
-    public class LengthMeasure
+    public class SliceUnit : NotifyPropertyChanged
     {
-        public Measure Measure { get; set; }
+        public SliceDirection Direction;
+        public Measure Measure;
+        public TickMark BigTickMark;
+        public TickMark SmallTickMark;
+
+        protected double[] units;
+
+        public double LongUnit;
+        public double ShortUnit;
+        public TickMark LongMark;
+        public TickMark ShortMark;
+
+        public void SetUnit(double maxLength)
+        {
+            ShortUnit = LongUnit = 0;
+            foreach (double longUnit in units)
+            {
+                if (longUnit < maxLength)
+                {
+                    ShortUnit = LongUnit;
+                    LongUnit = longUnit;
+                }
+            }
+        }
+    }
+
+    public class LengthMeasure : SliceUnit
+    {
+        new public Measure Measure 
+        {
+            get { return base.Measure; }
+            set 
+            { 
+                switch (value)
+                {
+                    case Measure.British:
+                        units = british;
+                        break;
+                    case Measure.Metric:
+                    default:
+                        units = metric;
+                        break;
+                }
+            } 
+        }
+
         private double[] metric = { 0.001, 0.01, 0.1, 1, 10, 100, 1000 };
         private double[] british = { };
+
+
+
     }
 }
