800bdf0edd6ec9e5f99ba8422c08781714533de4
DESKTOP-495NC0O\tdjlw <tdj.lwj@163.com>
2021-02-05 23:40:01 +0800

    2021-2-5

---
+++ tdjWpfClassLibrary/Converter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using tdjWpfClassLibrary.Draw;
+
+namespace tdjWpfClassLibrary
+{
+    public static class Converter
+    {
+        public static AxisDirection DirectionChange(AxisDirection direction)
+        {
+            switch (direction)
+            {
+                case AxisDirection.Horizontal:
+                    return AxisDirection.Vertical;
+                case AxisDirection.Vertical:
+                default:
+                    return AxisDirection.Horizontal;
+            }
+        }
+    }
+}
--- tdjWpfClassLibrary/Draw/NumberAxis.cs
+++ tdjWpfClassLibrary/Draw/NumberAxis.cs
@@ -13,19 +13,80 @@ namespace tdjWpfClassLibrary.Draw
     /// <summary>
     /// 刻度线类。
     /// </summary>
-    public class TickMark
+    public class TickMark : NotifyPropertyChanged
     {
-        public string Number;
-        public double Length;
-        public double X1, X2, Y1, Y2;
+        private double _x1, _x2, _y1, _y2;
+        public double X1
+        {
+            get => default;
+            set
+            {
+                if (value != _x1)
+                {
+                    _x1 = value;
+                    OnPropertyChanged("X1");
+                }
+            }
+        }
+
+        public double X2
+        {
+            get => default;
+            set
+            {
+                if (value != _x2)
+                {
+                    _x2 = value;
+                    OnPropertyChanged("X2");
+                }
+            }
+        }
+
+        public double Y1
+        {
+            get => default;
+            set
+            {
+                if (value != _y1)
+                {
+                    _y1 = value;
+                    OnPropertyChanged("Y1");
+                }
+            }
+        }
+
+        public double Y2
+        {
+            get => default;
+            set
+            {
+                if (value != _y2)
+                {
+                    _y2 = value;
+                    OnPropertyChanged("Y2");
+                }
+            }
+        }
     }
 
+    /// <summary>
+    /// 刻度，同样长度、单位的一组刻度线。
+    /// </summary>
     public class TickMarks : ObservableCollection<TickMark>
     {
         /// <summary>
         /// 刻度线方向，与数轴方向垂直。
         /// </summary>
         public AxisDirection Direction;
+
+        /// <summary>
+        /// 刻度线的相对于数轴单位的单位值。
+        /// </summary>
+        public double Unit;
+
+        /// <summary>
+        /// 刻度线的长度（像素）；
+        /// </summary>
         public double Length
         {
             get { return _length; }
@@ -51,25 +112,62 @@ namespace tdjWpfClassLibrary.Draw
         private double _length;
 
         /// <summary>
-        /// 添加刻度线，x，y为刻度线在坐标轴上的位置。length为刻度线的长度（像素）。
+        /// 数轴原点在画布上X轴坐标。
+        /// </summary>
+        public double X
+        {
+            get => default;
+            set
+            {
+            }
+        }
+
+        /// <summary>
+        /// 数轴原点在画布上Y轴坐标。
+        /// </summary>
+        public double Y
+        {
+            get => default;
+            set
+            {
+            }
+        }
+
+        /// <summary>
+        /// 构造函数需定义刻度线的方向、长度，并提供数轴原点的坐标。
+        /// </summary>
+        /// <param name="direction">刻度线的方向。</param>
+        /// <param name="length">刻度线的长度。</param>
+        /// <param name="x">数轴在画布上X轴的坐标。</param>
+        /// <param name="y">数轴在画布上Y轴的坐标。</param>
+        public TickMarks(AxisDirection direction, double length, double x, double y)
+        {
+            Length = length;
+            Direction = direction;
+            X = x;
+            Y = y;
+        }
+
+        /// <summary>
+        /// 添加刻度线，p为刻度线在坐标轴上的坐标值。length为刻度线的长度（像素）。
         /// </summary>
         /// <param name="x"></param>
         /// <param name="y"></param>
         /// <param name="length">刻度线的长度（像素）</param>
-        public void Add(double x, double y, double length)
+        public void Add(double p)
         {
             TickMark tick = new TickMark();
             switch (Direction)
             {
                 case AxisDirection.Horizontal:
-                    tick.X1 = x;
-                    tick.X2 = x + length;
-                    tick.Y1 = tick.Y2 = y;
+                    tick.X1 = X;
+                    tick.X2 = X + Length;
+                    tick.Y1 = tick.Y2 = p;
                     break;
                 case AxisDirection.Vertical:
-                    tick.X1 = tick.X2 = x;
-                    tick.Y1 = y;
-                    tick.Y2 = y - length;
+                    tick.X1 = tick.X2 = p;
+                    tick.Y1 = Y;
+                    tick.Y2 = Y - Length;
                     break;
             }
         }
@@ -80,66 +178,50 @@ namespace tdjWpfClassLibrary.Draw
         /// <summary>
         /// 数轴名称。
         /// </summary>
-        public string Title;
-        public AxisDirection Direction
-        {
-            get { return _direction; }
-            set
-            {
-                if (value != _direction)
-                {
-                    _direction = value;
-                    OnPropertyChanged("Direction");
-                    switch (_direction)
-                    {
-                        case AxisDirection.Horizontal:
-                            BigTicks.Direction = SmallTicks.Direction = AxisDirection.Vertical;
-                            break;
-                        case AxisDirection.Vertical:
-                            BigTicks.Direction = SmallTicks.Direction = AxisDirection.Horizontal;
-                            break;
-                    }
-                }
-            }
-        }
-        private AxisDirection _direction;
-        public TickMarks BigTicks;
-        public TickMarks SmallTicks;
+        private string Title;   //数轴单位名，例如：米，秒等。初始化时确定，其后不得更改。
+
+        private AxisDirection Direction; //数轴方向，初始化时确定，其后不得更改。
+        private AxisDirection TickDirection;
 
         /// <summary>
-        /// 数轴原点的位置。
+        /// 刻度线数组，内有多个刻度线。初始化时确定，其后不得修改。
         /// </summary>
-        public double X, Y;
+        public ObservableCollection<TickMarks> MultiTicks;  
 
-        public double BigDivide
+        public NumberAxis(AxisDirection direction, string title)
         {
-            get { return _bigDivide; }
+            Title = title;
+            Direction = direction;
+            TickDirection = Converter.DirectionChange(direction);
+        }
+
+        /// <summary>
+        /// 数轴原点在画布上的X方向的坐标。
+        /// </summary>
+        public double X
+        {
+            get => default;
             set
             {
-                if (value != _bigDivide)
-                {
-                    value = _bigDivide;
-                    OnPropertyChanged("BigDivide");
-                    foreach(var i in BigTicks)
-                    {
-                        switch (Direction)
-                        {
-                            case AxisDirection.Horizontal:
-                                i.Y2 = i.Y1 + value;
-                                break;
-                            case AxisDirection.Vertical:
-                                i.X2 = i.X1 + value;
-                                break;
-                        }
-                    }
-                }
             }
         }
-        public double SmallDivide;
-        private double _smallDivide, _bigDivide;
-        public TickMark LongMark;
-        public TickMark ShortMark;
 
+        /// <summary>
+        /// 数轴原点在画布上的Y方向坐标。
+        /// </summary>
+        public double Y
+        {
+            get => default;
+            set
+            {
+            }
+        }
+
+        public void AddTickMarks(TickMarks tickMarks)
+        {
+            tickMarks.Direction = TickDirection;
+            MultiTicks.Add(tickMarks);
+        }
 
     }
 }
