7a57287f755a209efbba955c97c87537fa33ff9c
DESKTOP-495NC0O\tdjlw <tdj.lwj@163.com>
2021-02-17 22:12:55 +0800

    重新建立profileDesigner项目。

--- OldProfileDesigner/App.xaml.cs
+++ OldProfileDesigner/App.xaml.cs
@@ -6,7 +6,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
 
-namespace profileDesigner
+namespace ProfileDesigner
 {
     /// <summary>
     /// Interaction logic for App.xaml
