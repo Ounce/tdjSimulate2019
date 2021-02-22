8f4b21a7ac80f0f195e089f534d8ff24c3acd8f6
Ounce <tdj.lwj@163.com>
2021-02-22 16:22:23 +0800

    事件处理函数与控件绑定混乱会影响事件窗口的显示。

--- ProfileDesigner/MainWindow.xaml.cs
+++ ProfileDesigner/MainWindow.xaml.cs
@@ -265,16 +265,18 @@ namespace profileDesigner
 
         }
 
+        private void ExistCanvas_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            MessageBox.Show("ExistCanvas_MouseDown");
+        }
+
         private void GradeCanvasRectangle_SizeChanged(object sender, SizeChangedEventArgs e)
         {
             Profiles.CanvasActualHeight = GradeCanvasRectangle.ActualHeight;
             Profiles.CanvasActualWidth = GradeCanvasRectangle.ActualWidth;
         }
 
-        private void ExistCanvas_MouseDown(object sender, MouseButtonEventArgs e)
-        {
-            MessageBox.Show("ExistCanvas_MouseDown");
-        }
+
 
         private void GradeCanvasRectangle_MouseDown(object sender, MouseButtonEventArgs e)
         {
