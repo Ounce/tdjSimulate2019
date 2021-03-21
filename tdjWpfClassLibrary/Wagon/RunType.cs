3ad3b4adc54aac878142592cf37e820ad3b0e175
tdjlwj <tdj.lwj@163.com>
2021-03-21 21:04:38 +0800

    定义RunType

---
+++ tdjWpfClassLibrary/Wagon/RunType.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace tdjWpfClassLibrary.Wagon
+{
+    /// <summary>
+    /// 走行性能分类。
+    /// </summary>
+    public enum RunType 
+    { 
+        [Description("易")]
+        Y = 1,
+        [Description("中")]
+        Z = 0,
+        [Description("难")]
+        N = -1,
+        [Description("极易")]
+        GY = 2,
+        [Description("极难")]
+        GN = -2
+    }
+
+    /// <summary>
+    /// 得到枚举的DescriptionAttribute值。
+    /// </summary>
+    public static class EnumHelper
+    {
+        static public string GetDescription(Enum en)
+        {
+            Type enumType = en.GetType();
+            var name = Enum.GetName(enumType, en);
+            if (name == null)
+                return string.Empty;
+            object[] objs = enumType.GetField(name).GetCustomAttributes(typeof(DescriptionAttribute), false);
+            if (objs == null || objs.Length == 0)
+                return string.Empty;
+            DescriptionAttribute attr = objs[0] as DescriptionAttribute;
+            return attr.Description;
+        }
+    }
+}
